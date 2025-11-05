c905cb32729262dedd73d16b02e17dae2d419ef0
DigiLeaf <leiforth01@hotmail.com>
2025-11-05 17:49:22 -0600

    can now read from json files and output to user

--- frontend/app.js
+++ frontend/app.js
@@ -2,44 +2,47 @@
 const searchbtn = document.getElementById("searchbtn");
 
 
-//Locates all results loaded on page and makes a collection
-const resultlist = document.getElementsByClassName("result");
-
-
-function handleSearch(event){
+//sends user search criteria to API 
+async function handleSearch(event){
     event.preventDefault()
-    //testing/sanity check
-    console.log("button clicked!")
+
+    //Gets value in search fields
     const citysearch = document.getElementById("citysearch").value;
     const foodsearch = document.getElementById("foodsearch").value;
 
-    const data = mockFetchData(citysearch,foodsearch)
+    //searches file/API based on criteria in search fields
+    const data = await FetchData(citysearch,foodsearch)
+
     displayResults(data);
 }
 
-//Test objects to generate mock data
-let restuarant ={name:"Angelos",style:"Italian"}
-let restuarant2 ={name:"Bop's",style:"Custard"}
-let restuarant3 ={name: "El Ranchito",style:"Mexican"}
 
-function mockFetchData(city,style){
-    let arr = [restuarant,restuarant2, restuarant3]
-    return arr;
+//Fetches restaurant Data based on user search criteria 
+async function FetchData(city, style){
+let arr = await fetch('rest.json')
+        .then((res)=>res.json())
+        .then((data)=> {console.log(data)
+            return data;
+        })
+        .catch((err) => {console.log(err)})
+
+return arr;
 }
 
-
+//Displays the results of the data passed in
 function displayResults(data){
     const resultsDiv = document.getElementById("search-results");
     //There should be not data in the HTML but removes just in case
     resultsDiv.innerHTML="";
+
     //Dynamically generate div based on search result
-    data.forEach(place =>{
+    data.forEach((place) => {
         const card = document.createElement("div");
         card.classList.add("result");
         card.innerHTML=`<h3>${place.name}</h3>
                         <p>${place.style}</p>`
         resultsDiv.appendChild(card)
-    })
+    });
 
 }
 
@@ -48,12 +51,10 @@ function searchOnClick(button){
 }
 
 
+
+
 //Adds event listener to search button
 searchOnClick(searchbtn)
-
-
-
-
 
 //debug purposes
 console.log("We made it we found the End!")
